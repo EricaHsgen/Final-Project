e020cf690328853a7d745d3bf953a375e80593a2
Erica Hoerschgen <eh0957939@otc.edu>
2025-04-28 08:51:49 -0500

    Initial Commit

---
+++ Final Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Final Project
+//
+//  Created by HOERSCHGEN, ERICA A. on 4/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
